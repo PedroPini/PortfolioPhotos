f0e60bccfcf76aa86a20949a59873e94e0eb3c78
PedroPini <augustopini@gmail.com>
2020-03-01 09:39:26 +0800

    adding new changes

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,16 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-mdx/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-theme-ui/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Emilia - @lekoarts/gatsby-theme-emilia","short_name":"Emilia","description":"Minimalistic portfolio/photography site with masonry grid, page transitions and big images. Themeable with Theme UI.","start_url":"/","background_color":"#fff","theme_color":"#3182ce","display":"standalone","icons":[{"src":"/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},{"src":"/android-chrome-512x512.png","sizes":"512x512","type":"image/png"}]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---node-modules-lekoarts-gatsby-theme-emilia-core-src-templates-projects-query-tsx": hot(preferDefault(require("D:\\www\\Photo\\PortfolioPhotos\\node_modules\\@lekoarts\\gatsby-theme-emilia-core\\src\\templates\\projects-query.tsx"))),
+  "component---node-modules-lekoarts-gatsby-theme-emilia-core-src-templates-project-query-tsx": hot(preferDefault(require("D:\\www\\Photo\\PortfolioPhotos\\node_modules\\@lekoarts\\gatsby-theme-emilia-core\\src\\templates\\project-query.tsx"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("D:\\www\\Photo\\PortfolioPhotos\\.cache\\dev-404-page.js")))
+}
+
